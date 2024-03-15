db4b3e8eea354e9358272cecad0681fa45ab4cbf
amesp73 <amitrpatel502@gmail.com>
2024-03-15 10:34:00 -0500

    Stored current theme in useEffect so if user rerenders the page, the theme will not revert back to the original light them but will search local storage for the last set theme.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 function App() {
 
+  //will search local storage for the current theme if available to store that
+  const currentTheme = localStorage.getItem('currentTheme');
+
   //initially website will be in lioght mode
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState(currentTheme ? currentTheme : 'light');
+
+  //whenever theme is updated with toggle icon it will execute this function
+  useEffect(() => {
+    localStorage.setItem('currentTheme', theme)
+  }, [theme])
 
   return (
     <div className={`App ${theme}`}>
