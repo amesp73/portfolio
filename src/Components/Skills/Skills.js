9490fe2cabf905d6c98f3ac9e7f6c8fbde03e15a
amesp73 <amitrpatel502@gmail.com>
2024-04-01 19:09:55 -0500

    COmpleted About section for portfolio. Included skills and descriptions as well as CSS

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -8,29 +8,29 @@ function Skills() {
   return (
     <section className='skills'>
         <span className='info'>What I Do</span>
-        <span className='infoExt'>I am a passionate, newly graduated web design student through Launchode
+        <span className='infoDesc'>I am a passionate, newly graduated web design student through Launchode
         with experience working with React, Java, and SQL to build functional and 
         visually appealing websites.</span>
-        <div className='skills'>
+        <div className='skillsBars'>
             <div className='skillBar'>
                 <img src={ReactIcon} alt='React' className='skillBarImg'/>
                 <div className='skillBarInfo'>
                     <h2>React</h2>
-                    <p></p>
+                    <p>Familiar with working with React for front end web development</p>
                 </div>
             </div>
             <div className='skillBar'>
                 <img src={SQL} alt='SQL' className='skillBarImg'/>
                 <div className='skillBarInfo'>
                     <h2>SQL</h2>
-                    <p></p>
+                    <p>Experience building databases for saving user info</p>
                 </div>
             </div>
             <div className='skillBar'>
                 <img src={Java} alt='Java' className='skillBarImg'/>
                 <div className='skillBarInfo'>
                     <h2>Java</h2>
-                    <p></p>
+                    <p>Experience with back end authentication and external API usage</p>
                 </div>
             </div>
         </div>
